41dcf37da30df32591eb8f57510282cdce01df7d
GJJ52019 <89346223+GJJ52019@users.noreply.github.com>
2021-11-08 21:22:16 -0500

    serializers updated to better represent the pages created

--- klng/serializers.py
+++ klng/serializers.py
@@ -4,18 +4,11 @@ from .models import User, Portfolio, Project
 
 #users serializer
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    portfolio = serializers.HyperlinkedRelatedField(
-        view_name='portfolio_detail',
-        read_only=True
-    )
-
-    project_url = serializers.ModelSerializer.serializer_url_field(
-        view_name='portfolio_detail')
-
+ 
     class Meta:
         model = User
         fields = (
-            'id','project_url','portfolio' ,'user_name', 'email', 'password')
+            'id','user_name', 'email', 'password')
 
 # portfolio serializer
 class PortfolioSerializer(serializers.HyperlinkedModelSerializer):
@@ -25,12 +18,12 @@ class PortfolioSerializer(serializers.HyperlinkedModelSerializer):
         read_only=True
     )
 
-    project_url = serializers.ModelSerializer.serializer_url_field(
-        view_name='project_detail')
+    portfolio_url = serializers.ModelSerializer.serializer_url_field(
+        view_name='portfolio_detail')
 
     class Meta:
         model = Portfolio
-        fields = ('id','name', 'about', 'github_link','linkedin_link','image_url','project_url','project') 
+        fields = ('id','name', 'about', 'github_link','linkedin_link','image_url','portfolio_url','project') 
 
 #Project serializer
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
@@ -43,7 +36,10 @@ class ProjectSerializer(serializers.HyperlinkedModelSerializer):
         queryset=Portfolio.objects.all(),
         source='portfolio'    
         )
+    
+    project_url = serializers.ModelSerializer.serializer_url_field(
+        view_name='project_detail')
 
     class Meta:
         model = Project
-        fields = ('id','portfolio_id', 'project_name', 'desc', 'project_link','image_url','portfolio') 
+        fields = ('id','portfolio_id', 'project_name', 'desc', 'project_link','image_url','portfolio','project_url') 
